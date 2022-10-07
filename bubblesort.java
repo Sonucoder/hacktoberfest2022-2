7517e290fe2e66899d1d8b3ffdcec0c27fedcef9
sonu kumar <89937675+Sonucoder@users.noreply.github.com>
2022-10-07 23:30:15 +0530

    bubblesort.java
    Add bubble sort program using java

---
+++ bubblesort.java
@@ -0,0 +1,69 @@
+// import java.util.*;
+// public class bubblesort{
+//     public static void printarray(int arr[]){
+//         for(int i=0;i<arr.length;i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//         System.out.println();
+//     }
+//     public static void main(String args[]){
+//         int arr[]={7,8,3,1,2};
+//         for(int i=0;i<arr.length-1;i++){
+//             for(int j=0;j<arr.length-i-1;j++){
+//                 if(arr[j]>arr[j+1]){
+//                     //swap condition use
+//                     int temp=arr[j];
+//                     arr[j]=arr[j+1];
+//                     arr[j+1]=temp;
+//                 }
+//             }
+//         }
+//         printarray(arr);
+//     }
+// }
+// import java.util.*;
+// public class bubblesort{
+//     public static void printarray(int arr[]){
+//         for(int i=0;i<arr.length;i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//         System.out.println();
+//     }
+// public  static void main(String args[]){
+//     int arr[]={2,3,1};
+//     for(int i=0;i<arr.length-1;i++){
+//         for(int j=0;j<arr.length-i-1;j++){
+//             if(arr[j]>arr[j+1]){
+//                 int temp=arr[j];
+//                 arr[j]=arr[j+1];
+//                 arr[j+1]=temp;
+//             }
+//         }
+//     }
+//     printarray(arr);
+    
+// }
+// }
+//bubble short
+import java.util.*;
+public class bubblesort{
+    public static void printarray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String n[]){
+        int arr[]={6,3,8,1};
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        printarray(arr);
+    }
+}
